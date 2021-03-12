506bad9c82607489ec65dafe559886693f5036f3
Muzosh <muzosak@seznam.cz>
2021-03-12 16:15:04 +0100

    Add library package and test file

---
+++ pqtest.py
@@ -0,0 +1,46 @@
+from secrets import compare_digest
+# from pqcrypto.kem.firesaber import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.frodokem1344aes import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.frodokem1344shake import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.frodokem640aes import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.frodokem640shake import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.frodokem976aes import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.frodokem976shake import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.kyber1024 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.kyber1024_90s import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.kyber512 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.kyber512_90s import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.kyber768 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.kyber768_90s import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.lightsaber import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece348864 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece348864f import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece460896 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece460896f import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece6688128 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece6688128f import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece6960119 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece6960119f import generate_keypair, encrypt, decrypt
+from pqcrypto.kem.mceliece8192128 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.mceliece8192128f import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.ntruhps2048509 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.ntruhps2048677 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.ntruhps4096821 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.ntruhrss701 import generate_keypair, encrypt, decrypt
+# from pqcrypto.kem.saber import generate_keypair, encrypt, decrypt
+
+# Alice generates a (public, secret) key pair
+public_key, secret_key = generate_keypair()
+
+# Bob derives a secret (the plaintext) and encrypts it with Alice's public key to produce a ciphertext
+ciphertext, plaintext_original = encrypt(public_key)
+
+print("Original plain text: \t", plaintext_original)
+
+# Alice decrypts Bob's ciphertext to derive the now shared secret
+plaintext_recovered = decrypt(secret_key, ciphertext)
+
+# Compare the original and recovered secrets in constant time
+assert compare_digest(plaintext_original, plaintext_recovered)
+
+print("Recovered plain text: \t", plaintext_recovered)
