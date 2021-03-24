e0774eb4be524fba832bdb138109f40225f8228d
Muzosh <muzosak@seznam.cz>
2021-03-24 15:23:50 +0100

    Add AES encryption

--- PqEncryptionManager.py
+++ PqEncryptionManager.py
@@ -30,6 +30,45 @@ from pqcrypto.kem.ntruhps2048509 import encrypt_ntruhps2048509, decrypt_ntruhps2
 from pqcrypto.kem.saber import encrypt_saber, decrypt_saber
 class PqEncryptionManager:
     
+    def aesEncrypt(self, dataBytes, symmetricKey):        
+        # generate a random salt
+        salt = get_random_bytes(AES.block_size)
+
+        # use the Scrypt KDF to get a private key from the 2x hashed masterPassword = PBKDF2 (Password Based Key Derivation Function)
+        private_key = hashlib.scrypt(
+            symmetricKey, salt=salt, n=2**14, r=8, p=1, dklen=32)
+
+        # GCM mode used for authenticated encryption --> authenticated tag
+        cipher_config = AES.new(private_key, AES.MODE_GCM)
+
+        # return a dictionary with the encrypted text
+        cipher_text, tag = cipher_config.encrypt_and_digest(dataBytes)
+        return {
+            'cipher_text': base64.b64encode(cipher_text).decode('utf-8'),
+            'salt': base64.b64encode(salt).decode('utf-8'),
+            'nonce': base64.b64encode(cipher_config.nonce).decode('utf-8'),
+            'tag': base64.b64encode(tag).decode('utf-8')
+        }
+    
+    def aesDecrypt(self, encryptedData, symmetricKey):
+        # decode the dictionary entries from base64
+        salt = base64.b64decode(encryptedData['salt'])
+        cipher_text = base64.b64decode(encryptedData['cipher_text'])
+        nonce = base64.b64decode(encryptedData['nonce'])
+        tag = base64.b64decode(enc_encryptedDatadict['tag'])
+        
+        # generate the private key from the 2x hashed masterPassword and salt
+        private_key = hashlib.scrypt(
+            symmetricKey, salt=salt, n=2**14, r=8, p=1, dklen=32)
+
+        # create the cipher config
+        cipher = AES.new(private_key, AES.MODE_GCM, nonce=nonce)
+
+        # decrypt the cipher text
+        decrypted = cipher.decrypt_and_verify(cipher_text, tag)
+        
+        return decrypted
+    
     #mceliece
     def encapsulation_mceliece8192128(public_key):
         ciphertext, secret_key = encrypt_mceliece8192128(public_key)
